eef41749e7abfffd2e5af9f9025b9dcfa19268e7
Leonardo Carvalho <leonardocarvalhosantos14@gmail.com>
2026-06-18 14:58:06 -0300

    feat: Adicionando Lombok e refatorando o codigo usando a dependencia.

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/Missao.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/Missao.java
@@ -2,54 +2,23 @@ package dev.java10x.CadastroDeNinjas.Missoes.Model;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.Model.Ninja;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "missao")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Missao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String dificuldade;
-
-    public Missao(String nome, String dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public Missao() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Missao setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public Missao setNome(String nome) {
-        this.nome = nome;
-        return this;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public Missao setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-        return this;
-    }
-
     // @OneToMany - uma missao tem varios ninjas
     @OneToMany(mappedBy = "missao")
     private List<Ninja> ninjas;
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Model/Ninja.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Model/Ninja.java
@@ -2,6 +2,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas.Model;
 
 import dev.java10x.CadastroDeNinjas.Missoes.Model.Missao;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
@@ -9,6 +10,9 @@ import java.util.List;
 // JPA = Java Persistence API
 @Entity
 @Table(name = "ninja")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ninja {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,52 +20,6 @@ public class Ninja {
     private String nome;
     private String email;
     private int idade;
-
-    public Ninja(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Ninja() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Ninja setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public Ninja setNome(String nome) {
-        this.nome = nome;
-        return this;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Ninja setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public Ninja setIdade(int idade) {
-        this.idade = idade;
-        return this;
-    }
-
     // @ManyToOne - um ninja tem uma unica missao
     @ManyToOne
     @JoinColumn(name = "missao_id") // Forain Key ou chave estrangeira
