f68deb865f92f2fba47e4fb4eecad6023f4b7eeb
Leonardo Carvalho <leonardocarvalhosantos14@gmail.com>
2026-06-26 15:11:28 -0300

    feat: Adicionando EndPoints em NinjaController

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
@@ -1,8 +1,7 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.java10x.CadastroDeNinjas.Ninjas.Model.Ninja;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +10,35 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    // Adicinar ninja (CREATE)
+    @PostMapping("/create")
+    public String createNinja() {
+        return "Ninja foi criada com sucesso";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/all")
+    public String allNinjas() {
+        return "Mostrar todos os ninjas";
+    }
+
+    // Procurar ninja por Id (READ)
+    @GetMapping("/findbyID")
+    public String findByIdNinja(@RequestParam int id) {
+        return "Mostrar ninja por ID";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/update")
+    public String updateNinja(@RequestBody Ninja ninja) {
+        return "Ninja foi atualizado com sucesso";
+    }
+
+    // Deletar ninja (DELETE)
+    @DeleteMapping("/delete")
+    public String deleteNinja(@RequestParam int id) {
+        return "Ninja foi deletado com sucesso";
     }
 }
